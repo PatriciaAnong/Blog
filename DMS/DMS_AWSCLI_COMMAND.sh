79ca9f78528a056da9c8a82b170b7a9f7a6c7d96
PAnong <patriciaanong@gmail.com>
2017-08-16 10:26:42 -0400

    Delete DMS_AWSCLI_COMMAND.sh

--- DMS/DMS_AWSCLI_COMMAND.sh
+++
@@ -1,8 +0,0 @@
-aws dms create-replication-task 
-  --replication-task-identifier dr-who-migration 
-  --source-endpoint-arn arn:aws:dms:us-east-1:1111111111111:endpoint:DOCTORWHOISAWOMAN01 
-  --target-endpoint-arn arn:aws:dms:us-east-1:111111111111:endpoint:YUPSHESUREIS02WOWZERS 
-  --replication-instance-arn arn:aws:dms:us-east-1:111111111111:rep:SOCRAZYTHATDOCTORWHOSIAWOMAN03 
-  --migration-type full-load 
-  --table-mappings file:////home/panong/DMS_TABLEMAPPINGS.json 
-  --replication-task-settings file:////home/panong/DMS_TASKSETTINGS.json
