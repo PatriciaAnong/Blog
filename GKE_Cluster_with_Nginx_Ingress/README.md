62e8a5e63d70bca1c88a4c51bf7f9177facc43ae
PAnong <patricia@patricia-anong.com>
2020-05-31 21:28:52 -0400

    Include README and update outputs

---
+++ GKE_Cluster_with_Nginx_Ingress/README.md
@@ -0,0 +1,3 @@
+Deploy Nginx Ingress and Cert Manager on a GKE Cluster using Terraform
+
+The corresponding blog post can be found at Patricia-Anong.com/Blog
--- GKE_Cluster_with_Nginx_Ingress/outputs.tf
+++ GKE_Cluster_with_Nginx_Ingress/outputs.tf
@@ -8,13 +8,16 @@ output cluster_endpoint {
 }
 
 output client_certificate {
+  sensitive   = true
   value       = module.gke.client_certificate
 }
 
 output client_key {
+  sensitive   = true
   value       = module.gke.client_key
 }
 
 output cluster_ca_certificate {
+  sensitive   = true
   value       = module.gke.cluster_ca_certificate
 }
